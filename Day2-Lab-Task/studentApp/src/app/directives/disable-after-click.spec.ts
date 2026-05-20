5c235cbeef854c46429039254cd373180b8cb3bd
Mohamed Salem <108944781+MoSalem149@users.noreply.github.com>
2026-05-21 01:55:03 +0300

    Delete Day2-Lab-Task/studentApp directory

--- Day2-Lab-Task/studentApp/src/app/directives/disable-after-click.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisableAfterClick } from './disable-after-click';
-
-describe('DisableAfterClick', () => {
-  it('should create an instance', () => {
-    const directive = new DisableAfterClick();
-    expect(directive).toBeTruthy();
-  });
-});
